a5dc4e63cce6d13207d9026408e1869a0e0ea065
Krishna Sharma <krishna1209.becse24@chitkara.edu.in>
2025-02-07 18:01:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,7 +4,7 @@ int main() {
     int a, b;
     char c;
     printf("");
-    scanf("%d %d *c%c", &a,&b,&c);
+    scanf("%d %d %*c%c", &a,&b,&c);
     switch(c){
         case '+':
             printf("%d",a+b);
